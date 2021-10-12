8c25d3e76a18c085fd04aed19bb1f570dcfea7bd
michael <michael.shypileu@gmail.com>
2021-10-12 15:14:26 +0300

    fix generics

--- server/generics.ts
+++ server/generics.ts
@@ -1,8 +1,21 @@
 import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
 
-export type QV<T, V> = (prisma: T, args2: V) => T & V
+export type TQctx = {
+  user_id: string | undefined
+  is_admin: boolean
+}
 
-export type PQV<T, V> = (args1: T, args2: V) => T
+type TQprops = {
+  prisma: PrismaClient
+  ctx: TQctx
+}
 
-export type PQVN<T, V> = (args1: T, args2: V) => V | null
+export type PQ<T> = (p: TQprops) => Promise<T>
+export type PQV<T, V> = (p: TQprops, vars: V) => Promise<T>
+export type PQVN<T, V> = (p: TQprops, vars: V) => Promise<T | null>
+
+// export type QV<T, V> = (prisma: T, args2: V) => T & V
+
+// export type PQV<T, V> = (args1: T, args2: V) => T
+
+// export type PQVN<T, V> = (args1: T, args2: V) => V | null
