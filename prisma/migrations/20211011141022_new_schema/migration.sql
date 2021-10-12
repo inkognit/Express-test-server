9d392184aa7d29bcf1065abebb91fe83290dae1f
michael <michael.shypileu@gmail.com>
2021-10-12 15:12:36 +0300

    new migrate

---
+++ prisma/migrations/20211011141022_new_schema/migration.sql
@@ -0,0 +1,49 @@
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN', 'WRITER');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "php_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "nick_name" TEXT NOT NULL,
+    "first_name" TEXT,
+    "last_name" TEXT,
+    "description" TEXT,
+    "email" TEXT NOT NULL,
+    "birthday" TIMESTAMP(3) NOT NULL,
+    "role" "Role" NOT NULL DEFAULT E'USER',
+    "avatar" TEXT NOT NULL DEFAULT E'',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserPass" (
+    "id" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "user_id" TEXT NOT NULL,
+
+    CONSTRAINT "UserPass_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Orders" (
+    "id" TEXT NOT NULL,
+    "descriptions" TEXT NOT NULL,
+
+    CONSTRAINT "Orders_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_nick_name_key" ON "User"("nick_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "UserPass_user_id_key" ON "UserPass"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "UserPass" ADD CONSTRAINT "UserPass_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
