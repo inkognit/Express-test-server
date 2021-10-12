0df20dbe0a6812399434585f1097c9522be4ccc1
michael <michael.shypileu@gmail.com>
2021-10-12 15:13:39 +0300

    edit types

--- pages/types.ts
+++ pages/types.ts
@@ -1,9 +1,40 @@
-export type TUserRole = {
-  ADMIN?: 'ADMIN'
-  USER: 'USER'
+import { Prisma } from '.prisma/client'
+
+export enum EUserRole {
+  admin = 'admin',
+  author = 'author',
+  editor = 'editor',
+  user = 'user',
 }
 
+type Q<T> = () => Promise<T>
+type QV<T, V> = (vars: V) => Promise<T>
+type QVN<T, V> = (vars: V) => Promise<T | null>
+
 // USERS
+export type TPageUsers_item = {
+  id?: string
+  created_at: any
+  nick_name: string
+  first_name: string
+  last_name: string
+  description: string
+  birthday: any
+  role?: EUserRole
+}
+export type TPageUsers = {
+  users: TPageUsers_item[]
+  countWhere: number
+  countAll: number
+}
+export type TPageUsers_vars = {
+  where?: Prisma.UserWhereInput | null
+  orderBy?: Prisma.UserOrderByWithRelationInput[] | null
+  take?: number | null
+  skip?: number | null
+}
+export type TPageUsers_load = QV<TPageUsers, TPageUsers_vars>
+
 export type TPageUser_item = {
   id?: string
   created_at: any
@@ -12,15 +43,11 @@ export type TPageUser_item = {
   last_name: string
   description: string
   birthday: any
-  role?: TUserRole
-  userPass: TUserPass
+  role?: EUserRole
+}
+export type TPageUser = {
+  user: TPageUser_item
 }
 export type TPageUser_vars = { id: string }
 
-export type TUserCreate_var = {}
-
-//USERS PASS
-export type TUserPass = {
-  id?: string
-  pass: string
-}
+export type TPageUser_load = QV<TPageUser, TPageUser_vars>
