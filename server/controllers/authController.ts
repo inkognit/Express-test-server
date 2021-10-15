d049fc4bd223f77ef2af615ea21f41f8a92aab95
michael <michael.shypileu@gmail.com>
2021-10-15 15:29:50 +0300

    stop controllers

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -1,67 +1,94 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+// import { PrismaClient } from '@prisma/client'
+// import { PQVN } from 'generics'
+// const prisma = new PrismaClient()
 
-class AuthController {
-  async getUser(req: any, res: any) {
-    try {
-      res.json('server to work!')
-    } catch (e) {
-      console.log(e)
-      res.status(400).json({ message: ' Registration error' })
-    }
-  }
-  async registration(req: any, res: any) {
-    try {
-      const { nick_name, pass } = req.body
-      const candidate = async (args: {
-        email: string
-        password: string
-        prisma: PrismaClient
-      }) => {
-        //извлечь юзера по логу
-        const user = await prisma.user.findUnique({
-          where: {},
-        })
-      }
-      if (!candidate) {
-        const createUser = async (args: {
-          email: string
-          prisma: PrismaClient
-        }) => {
-          const max_id = await args.prisma.user.findMany({
-            take: 1,
-            orderBy: { php_id: 'desc' },
-            select: { php_id: true },
-          })
-          const php_id = max_id[0].php_id + 1
-          const { prisma } = args
-          const user = await prisma.user.create({
-            data: {
-              email: args.email,
-              php_id,
-              nick_name: '',
-              birthday: new Date(),
-            },
-          })
-          return user
-        }
-      }
-      return res
-        .status(400)
-        .json({ message: 'Пользователь с таким Именем уже сущестует' })
-    } catch (e) {
-      console.log(e)
-      res.status(400).json({ message: ' Registration error' })
-    }
-  }
-  async login(req: any, res: any) {
-    try {
-    } catch (e) {
-      console.log(e)
-      res.status(400).json({ message: ' Registration error' })
-    }
-  }
-}
+// class AuthController {
+//   async getUser(req: any, res: any) {
+//     try {
+//       res.json('server to work!')
+//     } catch (e) {
+//       console.log(e)
+//       res.status(400).json({ message: ' Registration error' })
+//     }
+//   }
 
-export default new AuthController()
-// module.exports = new AuthController()
+//   async registration(req: any, res: any) {
+//     try {
+//       const { nick_name, pass } = req.body
+//       const candidate = async (args: {
+//         email: string
+//         password: string
+//         prisma: PrismaClient
+//       }) => {
+//         //извлечь юзера по логу
+//         const user = await prisma.user.findUnique({
+//           where: {},
+//         })
+//       }
+//       if (!candidate) {
+//         const createUser = async (args: {
+//           email: string
+//           prisma: PrismaClient
+//         }) => {
+//           type TCreateUser_db = PQVN<
+//             {
+//               id?: string | undefined
+//               created_at?: any
+//               nick_name: string
+//               first_name: string
+//               last_name: string
+//               description?: string | undefined
+//               birthday?: any
+//             },
+//             {
+//               nick_name: string
+//               first_name: string
+//               last_name: string
+//               description: string
+//               birthday: Date
+//               email: string
+//               password: string
+//             }
+//           >
+//           const createUser: TCreateUser_db = async ({ prisma }, args) => {
+//             const user = await prisma.user.create({
+//               data: {
+//                 nick_name: args.nick_name,
+//                 first_name: args.first_name,
+//                 last_name: args.last_name,
+//                 description: args.description,
+//                 birthday: args.birthday,
+//                 email: args.email,
+//                 password: {
+//                   create: {
+//                     password: args.password,
+//                   },
+//                 },
+//               },
+//             })
+//             return user
+//           }
+
+//           return createUser
+//         }
+//       }
+//       return res
+//         .status(400)
+//         .json({ message: 'Пользователь с таким Именем уже сущестует' })
+//     } catch (e) {
+//       console.log(e)
+//       res.status(400).json({ message: ' Registration error' })
+//     }
+//   }
+
+//   async login(req: any, res: any) {
+//     try {
+//     } catch (e) {
+//       console.log(e)
+//       res.status(400).json({ message: ' Registration error' })
+//     }
+//   }
+// }
+
+// export default new AuthController()
+// // module.exports = new AuthController()
