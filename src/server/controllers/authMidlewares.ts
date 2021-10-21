1f4d4b30e3c565b75275d6c5639f95f732dcb16a
michael <michael.shypileu@gmail.com>
2021-10-21 16:19:19 +0300

    add exit button

--- src/server/controllers/authMidlewares.ts
+++ src/server/controllers/authMidlewares.ts
@@ -1,27 +1,27 @@
-import { env } from 'process'
-import jwt, { Secret } from 'jsonwebtoken'
+// import { env } from 'process'
+// import jwt, { Secret } from 'jsonwebtoken'
 
-require('dotenv').config()
+// require('dotenv').config()
 
-export default (req: any, res: any, next: Function) => {
-  if (req.method === 'OPTION') {
-    next()
-  }
+// export default (req: any, res: any, next: Function) => {
+//   if (req.method === 'OPTION') {
+//     next()
+//   }
 
-  try {
-    const token = req.headers.authorization.split(' ')[1]
-    if (!token) {
-      res.status
-      return res.status(403).json({ message: 'Пользователь не авторизован!' })
-    }
-    const decodeDATA = jwt.verify(token, env.JWT_SECRET as Secret)
-    console.log('decodeDATA: ', decodeDATA)
-    next()
-  } catch (e) {
-    console.error(e)
-    return res.status(403).json({ message: 'Пользователь не авторизован!' })
-  }
+//   try {
+//     const token = req.headers.authorization.split(' ')[1]
+//     if (!token) {
+//       res.status
+//       return res.status(403).json({ message: 'Пользователь не авторизован!' })
+//     }
+//     const decodeDATA = jwt.verify(token, env.JWT_SECRET as Secret)
+//     console.log('decodeDATA: ', decodeDATA)
+//     next()
+//   } catch (e) {
+//     console.error(e)
+//     return res.status(403).json({ message: 'Пользователь не авторизован!' })
+//   }
 
-  console.log('start')
-  next()
-}
+//   console.log('start')
+//   next()
+// }
--- src/server/controllers/routes.ts
+++ src/server/controllers/routes.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { get, post } from './httpMethods'
+import { get, PATH, post } from './httpMethods'
 
 import { login, userCreate } from '../queries/user'
 // const authRouts = require('./authRouts')
@@ -10,23 +10,46 @@ router.use(cookieParser())
 
 router.use(get('/', 'accountPage/mainPage.html'))
 router.use(get('/about.html', 'accountPage/about.html'))
-router.use(get('/auth.html', 'accountPage/auth.html'))
+// router.use(get('/auth.html', 'accountPage/auth.html'))
 router.use(get('/registration.html', 'accountPage/registration.html'))
 router.use(get('/user.html', 'accountPage/user.html'))
 
 // router.use('/login', authRouts)
 
 router.use(post('/create_user', userCreate))
-router.use(post('/login', login))
+// router.use(post('/login', login))
 
-// router.post('/login', async (req, res) => {
-//   res.contentType('application/json')
-//   res.status(201)
+router.get('/auth.html', async (req, res) => {
+  res.contentType('text/html')
+  const user_id = req.cookies.auth
+  console.log('req.body: ', user_id)
+  res.status(200)
+  if (!user_id) {
+    res.sendFile(PATH(__dirname) + `/pages/accountPage/auth.html`)
+  } else {
+    res.send('Вы уже авторизированы!')
+  }
+})
 
-//   const token = login(req.body)
-//   console.log('token in routs: ', token)
-//   res.cookie('auth', token)
-//   res.send('ok')
-// })
+router.post('/exit', async (req, res) => {
+  console.log('before: ', req.body.user_id)
+
+  res.clearCookie('auth', {
+    domain: 'localhost',
+    expires: new Date(0),
+    path: '/',
+  })
+  res.sendStatus(200)
+
+  return console.log('after: ', req.body.user_id)
+})
+
+router.post('/login', async (req, res) => {
+  res.contentType('application/json')
+  res.status(201)
+  console.log('before: ', req)
+  await login(req, res)
+  console.log('after: ', req.body)
+})
 
 export default router
