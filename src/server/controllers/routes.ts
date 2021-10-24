cbe8dacf8789a8939bf36e0cb1c7b716be6be3ba
michael <michael.shypileu@gmail.com>
2021-10-24 12:52:49 +0300

    rename & clearing

--- src/server/controllers/routes.ts
+++ src/server/controllers/routes.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 import { get, PATH, post } from './httpMethods'
-
 import { login, userCreate } from '../queries/user'
+
 // const authRouts = require('./authRouts')
 var cookieParser = require('cookie-parser')
 const router = express.Router()
@@ -10,24 +10,26 @@ router.use(cookieParser())
 
 router.use(get('/', 'accountPage/mainPage.html'))
 router.use(get('/about.html', 'accountPage/about.html'))
-// router.use(get('/auth.html', 'accountPage/auth.html'))
 router.use(get('/registration.html', 'accountPage/registration.html'))
 router.use(get('/user.html', 'accountPage/user.html'))
 
-// router.use('/login', authRouts)
-
 router.use(post('/create_user', userCreate))
-// router.use(post('/login', login))
 
-router.get('/auth.html', async (req, res) => {
+router.get('/loginPage.html', async (req, res) => {
   res.contentType('text/html')
-  const user_id = req.cookies.auth
   res.status(200)
+  const user_id = req.cookies.auth
   if (!user_id) {
-    res.sendFile(PATH(__dirname) + `/pages/accountPage/auth.html`)
+    res.sendFile(PATH(__dirname) + `/pages/accountPage/loginPage.html`)
   } else {
     res.send('Вы уже авторизированы!')
   }
+})
+
+router.post('/login', async (req, res) => {
+  res.contentType('application/json')
+  res.status(201)
+  await login(req, res)
 })
 
 router.post('/exit', async (req, res) => {
@@ -36,13 +38,7 @@ router.post('/exit', async (req, res) => {
     expires: new Date(0),
     path: '/',
   })
-  res.sendStatus(200)
-})
-
-router.post('/login', async (req, res) => {
-  res.contentType('application/json')
-  res.status(201)
-  await login(req, res)
+  res.sendStatus(201)
 })
 
 export default router
