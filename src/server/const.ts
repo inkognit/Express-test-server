2c0f29ad1862c9a7db1b35afee8c7114e199a7ff
michael <michael.shypileu@gmail.com>
2021-11-15 13:43:50 +0300

    user login refactor

--- src/server/const.ts
+++ src/server/const.ts
@@ -1,1 +1,18 @@
-export const AUTHNAME = 'auth'
+export const AUTHNAME = "auth";
+
+export const SYSTEM_MESSAGE = {
+  ok: {
+    statusCode: 200,
+    message: "OK!",
+  },
+  error_pass: {
+    code: "INVALID_PASS",
+    statusCode: 403,
+    message: "Неправильно введен Пароль!",
+  },
+  error_log: {
+    code: "INVALID_LOG",
+    statusCode: 403,
+    message: "Неправильно введен Логин!",
+  },
+};
--- src/server/queries/user.ts
+++ src/server/queries/user.ts
@@ -1,43 +1,29 @@
-import {
-  TPageUsers,
-  TPageUsers_vars,
-  TPageUser_item,
-  TPageUser_vars,
-} from '../../pages/types'
-import { PrismaClient } from '.prisma/client'
-import { PQV, PQVN } from '../generics'
-import bcrypt from 'bcryptjs'
-import jwt, { Secret } from 'jsonwebtoken'
-import { env } from 'process'
-import { check } from 'express-validator'
-import express from 'express'
-import cookieParser from 'cookie-parser'
-import { AUTHNAME } from '../const'
-const router = express.Router()
+import { PrismaClient } from ".prisma/client";
+import bcrypt from "bcryptjs";
+import cookieParser from "cookie-parser";
+import express from "express";
+import jwt, { Secret } from "jsonwebtoken";
+import { env } from "process";
+import { TPageUsers, TPageUsers_vars, TPageUser_item } from "../../pages/types";
+import { AUTHNAME, SYSTEM_MESSAGE } from "../const";
+import { PQV, PQVN } from "../generics";
+const router = express.Router();
 
-router.use(cookieParser())
-require('dotenv').config()
+router.use(cookieParser());
+require("dotenv").config();
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-export const generatorJWTToken = (
-  user_id: string,
-  roles: 'ADMIN' | 'USER' | 'WRITER',
-  nick_name: string,
-  email: string,
-) => {
+export const generatorJWTToken = (user_id: string) => {
   const payload = {
     user_id,
-    roles,
-    nick_name,
-    email,
-  }
+  };
   return jwt.sign(payload, env.JWT_SECRET as Secret, {
-    expiresIn: '24h',
-  })
-}
+    expiresIn: "24h",
+  });
+};
 
-export type TPageUsers_db = PQV<TPageUsers, TPageUsers_vars>
+export type TPageUsers_db = PQV<TPageUsers, TPageUsers_vars>;
 export const users: TPageUsers_db = async (args) => {
   const [users, countWhere, countAll] = await Promise.all([
     prisma.user.findMany({
@@ -59,12 +45,12 @@ export const users: TPageUsers_db = async (args) => {
       where: args.where || undefined,
     }),
     prisma.user.count(),
-  ])
-  return { users, countWhere, countAll }
-}
+  ]);
+  return { users, countWhere, countAll };
+};
 
 export const user = async (req: any, res: any) => {
-  const { user_id } = req.body
+  const { user_id } = req.body;
   const user = await prisma.user.findUnique({
     where: { id: user_id },
     select: {
@@ -74,35 +60,35 @@ export const user = async (req: any, res: any) => {
       last_name: true,
       birthday: true,
     },
-  })
-  return user
-}
+  });
+  return user;
+};
 
 export type TUserCreate_db = PQVN<
   TPageUser_item,
   {
-    nick_name: string
-    first_name: string
-    last_name: string
-    description: string
-    birthday: Date | string
-    email: string
-    pass: any
+    nick_name: string;
+    first_name: string;
+    last_name: string;
+    description: string;
+    birthday: Date | string;
+    email: string;
+    pass: any;
   }
->
+>;
 export const userCreate = async (
   req: {
     body: {
-      nick_name: string
-      first_name: string | ''
-      last_name: string | ''
-      description: string | ''
-      birthday: Date
-      email: string
-      pass: string
-    }
+      nick_name: string;
+      first_name: string | "";
+      last_name: string | "";
+      description: string | "";
+      birthday: Date;
+      email: string;
+      pass: string;
+    };
   },
-  res: any,
+  res: any
 ) => {
   const {
     nick_name,
@@ -112,29 +98,29 @@ export const userCreate = async (
     birthday,
     email,
     pass,
-  } = req.body
+  } = req.body;
   const existUser = await prisma.user.findUnique({
     where: { nick_name },
-  })
+  });
   if (existUser) {
-    res.status(455).json({ error: 'Такой nickname уже существует!' })
+    res.status(455).json({ error: "Такой nickname уже существует!" });
   }
 
   const existEmail = await prisma.user.findUnique({
     where: { email },
-  })
+  });
   if (existEmail) {
-    res.status(456).json({ error: 'Такой email уже существует!' })
+    res.status(456).json({ error: "Такой email уже существует!" });
   }
 
-  if (pass.match(/^[a-zA-Z0-9]{5,15}$/)) {
+  if (!pass.match(/^[a-zA-Z0-9]{5,15}$/)) {
     res.status(457).json({
       error:
-        'Пароль должен быть от 5 до 15 символов и содержать буквы латинского алфавита и цифры!',
-    })
+        "Пароль должен быть от 5 до 15 символов и содержать буквы латинского алфавита и цифры!",
+    });
   }
 
-  const encryptedPassword = await bcrypt.hash(pass, 10)
+  const encryptedPassword = await bcrypt.hash(pass, 10);
   const user = await prisma.user.create({
     data: {
       nick_name,
@@ -149,18 +135,18 @@ export const userCreate = async (
         },
       },
     },
-  })
-  return user
-}
+  });
+  return user;
+};
 
 export const login = async (req: any, res: any) => {
-  const { login, pass } = req.body
-  let email, nick_name
+  const { login, pass } = req.body;
+  let email, nick_name;
 
   if (login.match(/^[^\s@]+@[^\s@]+$/)) {
-    email = login
+    email = login;
   } else {
-    nick_name = login
+    nick_name = login;
   }
 
   const user = await prisma.user.findUnique({
@@ -171,39 +157,28 @@ export const login = async (req: any, res: any) => {
       nick_name: true,
       email: true,
     },
-  })
+  });
   if (user) {
     const password = await prisma.userPass.findUnique({
       where: { user_id: user.id },
       select: { password: true },
-    })
+    });
     if (password?.password) {
       if (bcrypt.compareSync(pass, password.password)) {
-        const tokenClear = generatorJWTToken(
-          user.id,
-          user.role,
-          user.nick_name,
-          user.email,
-        )
-        const token = `${tokenClear}`
-        res.cookie(AUTHNAME, token)
-        console.log('токен создан')
-        return res.json({ token })
+        const tokenClear = generatorJWTToken(user.id);
+        const token = `${tokenClear}`;
+        res.cookie(AUTHNAME, token);
+        console.log("токен создан");
+        return res.json({ token });
       } else {
-        res.status(466).json({
-          message: 'bad pass',
-          error: 'Неправильно введен пароль!  !!! !!!',
-        })
-        throw new Error('Неправильно введен пароль!  !!! !!!')
+        res.status(403).json(SYSTEM_MESSAGE.error_pass);
+        throw new Error("Неправильно введен пароль!");
       }
     }
   } else {
-    res.status(465).json({
-      message: 'bad log',
-      error: 'Неправильно введен login!  !!! !!!',
-    })
+    res.status(403).json(SYSTEM_MESSAGE.error_log);
     throw new Error(
-      'Вы ввели неправильный логин или вашего аккаунта не существует!',
-    )
+      "Вы ввели неправильный логин или вашего аккаунта не существует!"
+    );
   }
-}
+};
