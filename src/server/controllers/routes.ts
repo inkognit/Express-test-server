9294f228ea42cb04903e1e613cecd6fa70816170
michael <michael.shypileu@gmail.com>
2021-12-19 17:29:46 +0300

    refactor regostration page

--- src/server/controllers/routes.ts
+++ src/server/controllers/routes.ts
@@ -9,7 +9,7 @@ const router = express.Router();
 router.use(cookieParser());
 
 router.use(getClear("/", "accountPage/mainPage.html"));
-router.use(getClear("/registration", "accountPage/registration.html"));
+// router.use(getClear("/registration", "accountPage/registration.html"));
 
 router.use(get("/user", user, "user"));
 
@@ -17,4 +17,10 @@ router.use(post("/create_user", userCreate));
 router.use(post("/user_up", user_up));
 router.use(post("/login", login));
 
+router.get("/registration", async (req, res) => {
+  res.render("pages/registration", {
+    title: "Registration",
+    active: "registration",
+  });
+});
 export default router;
