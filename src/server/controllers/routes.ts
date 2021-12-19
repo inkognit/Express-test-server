6ba08b07fa8f474eb6677a2aaafe772055c65935
michael <michael.shypileu@gmail.com>
2021-12-19 18:00:24 +0300

    refactor routers

--- src/server/controllers/routes.ts
+++ src/server/controllers/routes.ts
@@ -2,33 +2,19 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import { login, userCreate } from "../queries/user";
 import { user, user_up } from "../queries/users";
-import { get, post } from "./httpMethods";
+import { get, getClear, post } from "./httpMethods";
 
 const router = express.Router();
 
 router.use(cookieParser());
 
-// router.use(getClear("/", "accountPage/mainPage.html"));
-// router.use(getClear("/registration", "accountPage/registration.html"));
+router.use(getClear("/", "main", "Main"));
+router.use(getClear("/registration", "registration", "Registration"));
 
 router.use(get("/user", user, "user"));
 
 router.use(post("/create_user", userCreate));
 router.use(post("/user_up", user_up));
 router.use(post("/login", login));
-
-router.get("/registration", async (req, res) => {
-  res.render("pages/registration", {
-    title: "Registration",
-    active: "registration",
-  });
-});
-
-router.get("/", async (req, res) => {
-  res.render("pages/main", {
-    title: "Main",
-    active: "main",
-  });
-});
 
 export default router;
